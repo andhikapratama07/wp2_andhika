b923aab38ad38e4c5a8edaa33f60946339167695
andhikapratama07 <tampandhika3@gmail.com>
2023-12-08 22:43:34 -0800

    P01-P12

---
+++ application/controllers/Contoh2.php
@@ -0,0 +1,14 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalan Diri</h1>";
+        echo "Nama : Andhika pratama <br>";
+        echo "Nim  : 12220828 <br>";
+        echo "Kelas: 12.3A.05 <br>";
+        echo "TTL  : Bogor, 27 Juli 2004 <br>";
+        echo "Hobi : Bermain Games Dan Olahraga"; 
+        //contoh 1.php//
+    }
+}
